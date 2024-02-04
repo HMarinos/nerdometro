c8b86e548b650542a0bdc88b1719a04204ea83e9
marinos hotza <marinos1366@gmail.com>
2024-02-04 18:00:20 +0200

    mini-fix

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -11,8 +11,6 @@ class MediaController extends Controller
         $apiKEY = '05abd598284193009c38291a6823dd0c';
         $response = Http::get('https://api.themoviedb.org/3/movie/popular', ['api_key' => $apiKEY])->json();
         dd($response);
-        // Pass the response to the view
-        return view('category');
     }
 
     function fetchAndStoreAnime(){
@@ -26,17 +24,8 @@ class MediaController extends Controller
     }
 
     function fetchAndStoreGames(){
-        // $ttv_response = Http::post('https://id.twitch.tv/oauth2/token?client_id=bc6zobl5qlfnzw2xhk79cyty02sjmj&client_secret=nfy3sswj9zq23n2i9e28s48tu23ri4&grant_type=client_credentials')->json();
-        // $ttv_token = $ttv_response['access_token'];
-        // $ttv_client_id = 'bc6zobl5qlfnzw2xhk79cyty02sjmj';
-        // $igdb_response = Http::withHeaders([
-        //     'Client-ID'=>$ttv_client_id,
-        //     'Authorization'=>'Bearer '.$ttv_token,
-        // ])->post('https://api.igdb.com/v4/games')->json();
-        // dd($ttv_token,$ttv_client_id,$igdb_response);
         $api_key = '925517f17a024b508da64ad9f4d7e388';
         $response = Http::get("https://api.rawg.io/api/games?key={$api_key}")->json();
-
         dd($response);
     }
 }
