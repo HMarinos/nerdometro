66e94e20e12cfcae0a61a6163ea93735187420ca
HMarinos <100523671+HMarinos@users.noreply.github.com>
2024-03-17 19:03:49 +0200

    get tmdb trailer

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -9,11 +9,28 @@ class MovieController extends Controller
 {
     function showSingleMovie($id){
         $apiKey = '05abd598284193009c38291a6823dd0c';
-        $response = Http::get('https://api.themoviedb.org/3/movie/19404', [
+        $response = Http::get('https://api.themoviedb.org/3/movie/'. $id, [
+            'api_key' => $apiKey
+        ])->json();
+        $response_video = Http::get('https://api.themoviedb.org/3/movie/' . $id . '/videos', [
             'api_key' => $apiKey
         ])->json();
         
         dump($response);
-        return view('singleMovie',['movie'=>$response]);
+        // dump($response_video['results']);
+
+        $objects = $response_video['results'];
+        
+        $firstTrailerObject = null;
+
+        foreach ($objects as $object) {
+            if ($object['type'] === "Trailer") {
+                $firstTrailerObject = $object;
+                break; 
+            }
+        }
+        dump($firstTrailerObject);
+        
+        return view('singleMovie',['movie'=>$response,'video'=>$firstTrailerObject]);
     }
 }
--- resources/views/singleMovie.blade.php
+++ resources/views/singleMovie.blade.php
@@ -1,1 +1,29 @@
-<h1>Movies right here !!</h1>
+<section>
+    <img src="" alt="">
+    <div class="info">
+        <h1>{{$movie['title']}}</h1>
+        <div>
+            <ul>
+                <li>{{$movie['release_date']}}</li>
+                <li>{{$movie['runtime']}} minutes</li>
+                <li>{{$movie['vote_average']}}/10</li>
+            </ul>
+        </div>
+    <div>
+    <div class="media">
+        <img src="{{ 'https://image.tmdb.org/t/p/w500/' . $movie['poster_path'] }}" alt="">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/{{$video['key']}}?si=-MvG6LyMyhOJ7Kir" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+    <div class="details">
+        <ul class="genres">
+            @foreach ($movie['genres'] as $genre)
+                <li>{{$genre['name']}}</li>
+            @endforeach
+        </ul>
+        <ul>
+            <li></li>
+            <li></li>
+        </ul>
+        <div>{{$movie['overview']}}</div>
+    </div>
+</section>
