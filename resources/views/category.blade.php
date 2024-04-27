9b5f173c2dd812ce37b323226162932f66af8fb4
marinos hotza <marinos1366@gmail.com>
2024-04-27 15:23:36 +0300

    2704

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -1,22 +1,31 @@
-<section>
-    @php
-    $category = Route::current()->parameter('category');
-    @endphp
+<x-app-layout>
+    
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <section>
+        @php
+        $category = Route::current()->parameter('category');
+        @endphp
 
 
-    <h1>My {{$category}} library</h1>
-    <ul>
-        <li>add watched {{$category}} </li>
-        <li>add to watch {{$category}} </li>
-    </ul>
+        <h1>My {{$category}} library</h1>
+        <ul>
+            <li>add watched {{$category}} </li>
+            <li>add to watch {{$category}} </li>
+        </ul>
 
-    <div class="list">
-        <div class="flex">
-            @component('components.media-card')
-            @endcomponent
+        <div class="list">
+            <div class="flex">
+                @component('components.media-card')
+                @endcomponent
+            </div>
         </div>
-    </div>
-    <form method="GET" action="">
-        <input type="text" placeholder="search.." name="search"> 
-    </form>
-</section>
+        <form method="GET" action="">
+            <input type="text" placeholder="search.." name="search"> 
+        </form>
+    </section>
+</x-app-layout>
